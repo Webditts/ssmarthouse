b8886fa3047a8abc882c377a9408e06f266d8d86
lewis-andy <lewisandanje3@gmail.com>
2025-03-28 13:51:10 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/smarthouse/SmartHouse.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.smarthouse;
+
+/**
+ *
+ * @author lewis
+ */
+public class SmartHouse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
